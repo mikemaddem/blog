d8f8057cc8f489f5bb9b21fb683d27ca45b2be2e
Mike M <mikemaddem@users.noreply.github.com>
2022-10-04 10:39:33 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        //icon: `src/images/csc-logo.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
